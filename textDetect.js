c587cc854237bd99ebe93ea3af11abfc18b94792
unknown <swm10099@naver.com>
2020-05-03 14:30:27 +0900

    텍스트추출

---
+++ textDetect.js
@@ -0,0 +1,23 @@
+'use strict';
+
+async function detectText(fileName) {
+    // [START vision_text_detection]
+    const vision = require('@google-cloud/vision');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    //fileName = '이미지파일 경로';
+
+    // Performs text detection on the local file
+    const [result] = await client.textDetection(fileName);
+    const detections = result.textAnnotations;
+    console.log('Text:');
+    detections.forEach(text => console.log(text));
+    // [END vision_text_detection]
+}
+
+detectText().catch(console.error);
